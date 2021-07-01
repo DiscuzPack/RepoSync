f7dc522ab9e73ce89b62bba8d0108945b465e449
老周部落 <laozhoubuluo@126.com>
2021-07-02 00:20:51 +0800

    优化 更明确更多的信息展示和日志

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
+# Show Running Command
+set -x
+
 # Input SSH Key
 mkdir -p /root/.ssh
 echo "${INPUT_PRIVATE_KEY}" > /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
+
+# Git Debug Mode
+GIT_TRACE=2
+GIT_CURL_VERBOSE=2
+GIT_TRACE_PERFORMANCE=2
+GIT_TRACE_PACK_ACCESS=2
+GIT_TRACE_PACKET=2
+GIT_TRACE_PACKFILE=2
+GIT_TRACE_SETUP=2
+GIT_TRACE_SHALLOW=2
 
 # Pull Upstream Repo
 mkdir -p /root/repo/
